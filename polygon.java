5b7da8f9cd0282831923243397426015b5d0271c
hoo7014 <98494038+hoo7014@users.noreply.github.com>
2022-02-01 08:24:50 -0600

    Create polygon.java

---
+++ polygon.java
@@ -0,0 +1,18 @@
+-/SwelteringImpoliteAmoebas$ javac Polygon.java
+-/SwelteringImpoliteAmoebas$ java Polygon
+Do you want to add a side?[Y/N]
+Y
+Enter side:10
+Do you want to add a side?[Y/N]
+Y
+Enter side:15
+Do you want to add a side?[Y/N]
+Y
+Enter side:30
+Do you want to add a side?[Y/N]
+Y
+Enter side:20
+Do you want to add a side?[Y/N]
+N
+Number of the sides add = 4
+Perimter of the polygon =75.0
